ffc9fcb8b5facd076634aefa2a68ae865620f587
Arturo Romero <ajruwork@gmail.com>
2023-11-28 00:59:14 -0500

    se termina footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
 import { HomeRecipesComponent } from './components/home-recipes/home-recipes.component';
 import { HomeTeamComponent } from './components/home-team/home-team.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeBannerComponent, HomeRecipesComponent, HomeTeamComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeBannerComponent, HomeRecipesComponent, HomeTeamComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
