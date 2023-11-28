288688cba8f6da6bcc9c5d5f53dfcee266e6e65c
Arturo Romero <ajruwork@gmail.com>
2023-11-27 19:14:33 -0500

    se configura seccion de platos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { HomeRecipesComponent } from './components/home-recipes/home-recipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeBannerComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeBannerComponent, HomeRecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
