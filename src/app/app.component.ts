4f15daebb9576ac8890f21f01f2e90af0fd34ebc
Arturo Romero <ajruwork@gmail.com>
2023-11-27 20:34:50 -0500

    se configura seccion de equipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
 import { HomeRecipesComponent } from './components/home-recipes/home-recipes.component';
+import { HomeTeamComponent } from './components/home-team/home-team.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeBannerComponent, HomeRecipesComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeBannerComponent, HomeRecipesComponent, HomeTeamComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
